edd97455e3588024d6c821d99c20e414d5ed0d11
Evan Vujcec <evanvujcec@gmail.com>
2018-03-29 17:26:44 -0400

    ngrok to merge

---
+++ src/environments/environment.ngrok.ts
@@ -0,0 +1,20 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment: any = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCQjwx9ZU8LEtUrrsoiJ69RTprvnlplBKk',
+    authDomain: 'relational-image-map-dev.firebaseapp.com',
+    databaseURL: 'https://relational-image-map-dev.firebaseio.com',
+    projectId: 'relational-image-map-dev',
+    storageBucket: 'relational-image-map-dev.appspot.com',
+    messagingSenderId: '453965883992'
+  },
+  appEngine: {
+    search: 'search.relational-image-map-dev.appspot.com'
+  },
+  googleMapsKey: 'AIzaSyBaAYO4oNtD4nfrzGSf7m_l7wbRhVt1sWo'
+};
