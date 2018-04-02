0232d09cec829fa6cfdb488e244bb49eaf47d200
Evan Vujcec <evanvujcec@gmail.com>
2018-04-01 21:11:14 -0400

    Ngrok prod

---
+++ src/environments/environment.ngrok.prod.ts
@@ -0,0 +1,15 @@
+export const environment: any = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCQjwx9ZU8LEtUrrsoiJ69RTprvnlplBKk',
+    authDomain: 'relational-image-map-dev.firebaseapp.com',
+    databaseURL: 'https://relational-image-map-dev.firebaseio.com',
+    projectId: 'relational-image-map-dev',
+    storageBucket: 'relational-image-map-dev.appspot.com',
+    messagingSenderId: '453965883992'
+  },
+  appEngine: {
+    search: 'search.relational-image-map-dev.appspot.com'
+  },
+  googleMapsKey: 'AIzaSyBaAYO4oNtD4nfrzGSf7m_l7wbRhVt1sWo'
+};
